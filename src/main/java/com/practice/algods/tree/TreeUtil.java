c97f600811a048f0946b5a0c364b123922cbb1c6
Kuldeep Tiwari <kutiwari@castlighthealth.com>
2020-03-29 17:30:31 +0530

    removed redundant code

--- src/main/java/com/practice/algods/tree/TreeUtil.java
+++ src/main/java/com/practice/algods/tree/TreeUtil.java
@@ -5,8 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TreeUtil {
-  private static final Comparable[] treeData = new Integer[]{10,9,8,3,4,5,6,2,1,0,3};
+
   public static List<Comparable> treeDataFromInOrderWalk = new ArrayList<>();
+
+  /**
+   *
+   * @param treeData - unsorted data
+   * @return
+   */
+  public static List<Comparable> sortedDataFromInOrderWalk(Comparable[] treeData) {
+    TreeNode root = createTree(treeData);
+    inOrder(root);
+    return treeDataFromInOrderWalk;
+  }
+
   private static TreeNode createTree(Comparable[] treeData) {
     Comparable[] sortedTreeData = new QuickSort().sort(treeData);
     TreeNode root = createTreeFromSortedArray(sortedTreeData, 0, sortedTreeData.length-1);
@@ -22,12 +34,6 @@ public class TreeUtil {
     root.setLeftChild(createTreeFromSortedArray(array, start, mid-1));
     root.setRightChild(createTreeFromSortedArray(array, mid+1, end));
     return root;
-  }
-
-  public static List<Comparable> sortedDataFromInOrderWalk(Comparable[] treeData) {
-    TreeNode root = createTree(treeData);
-    inOrder(root);
-    return treeDataFromInOrderWalk;
   }
 
     private static void inOrder (TreeNode<Comparable> root) {
